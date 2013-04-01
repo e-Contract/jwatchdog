f1f70092bdf2373da13d68990f7d0f7c68c8635a
Frank Cornelis <info@e-contract.be>
2013-04-01 11:26:16 +0200

    scripts: injected variables now live under a jwatchdog object

---
+++ jwatchdog-core/src/main/java/be/e_contract/jwatchdog/ScriptObject.java
@@ -0,0 +1,33 @@
+/*
+ * Java Watchdog Project.
+ * Copyright (C) 2013 Frank Cornelis.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.e_contract.jwatchdog;
+
+import be.e_contract.jwatchdog.notifier.Notifier;
+
+public class ScriptObject {
+
+	public final double[] values;
+
+	public final Notifier notifier;
+
+	public ScriptObject(double[] values, Notifier notifier) {
+		this.values = values;
+		this.notifier = notifier;
+	}
+}
