afee92e7a68f343708b472984d4f3daf167df719
Frank Cornelis <fcorneli@users.noreply.github.com>
2013-04-04 21:35:28 +0200

    IRC notifier

---
+++ jwatchdog-notifier-irc/src/main/java/be/e_contract/jwatchdog/notifier/irc/IRCNotifier.java
@@ -0,0 +1,83 @@
+/*
+ * Java Watchdog Project.
+ * Copyright (C) 2013 Frank Cornelis.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.e_contract.jwatchdog.notifier.irc;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.pircbotx.PircBotX;
+import org.pircbotx.exception.IrcException;
+import org.pircbotx.exception.NickAlreadyInUseException;
+
+import be.e_contract.jwatchdog.Context;
+import be.e_contract.jwatchdog.notifier.Notifier;
+
+public class IRCNotifier implements Notifier {
+
+	private static final Log LOG = LogFactory.getLog(IRCNotifier.class);
+
+	private final String server;
+
+	private final String name;
+
+	private final String channel;
+
+	public IRCNotifier(String server, String name, String channel) {
+		this.server = server;
+		this.name = name;
+		this.channel = channel;
+	}
+
+	@Override
+	public void init(Context context) {
+		// empty
+	}
+
+	@Override
+	public void notify(String message) {
+		LOG.debug("IRC notification: " + message);
+		PircBotX pircBotX = new PircBotX();
+		pircBotX.setName(this.name);
+		NotifierBot notifierBot = new NotifierBot(message);
+		pircBotX.getListenerManager().addListener(notifierBot);
+		LOG.debug("connecting to IRC server...");
+		try {
+			pircBotX.connect(this.server);
+		} catch (NickAlreadyInUseException e) {
+			LOG.error("nick already used: " + this.name);
+			return;
+		} catch (IOException e) {
+			LOG.error("IO error: " + e.getMessage());
+			return;
+		} catch (IrcException e) {
+			LOG.debug("IRC error: " + e.getMessage());
+			return;
+		}
+		pircBotX.joinChannel(this.channel);
+
+		try {
+			synchronized (notifierBot) {
+				notifierBot.wait();
+			}
+		} catch (InterruptedException e) {
+			LOG.error("wait error: " + e.getMessage());
+		}
+	}
+}
---
+++ jwatchdog-notifier-irc/src/main/java/be/e_contract/jwatchdog/notifier/irc/NotifierBot.java
@@ -0,0 +1,61 @@
+/*
+ * Java Watchdog Project.
+ * Copyright (C) 2013 Frank Cornelis.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.e_contract.jwatchdog.notifier.irc;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.pircbotx.Channel;
+import org.pircbotx.PircBotX;
+import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.ConnectEvent;
+import org.pircbotx.hooks.events.DisconnectEvent;
+import org.pircbotx.hooks.events.JoinEvent;
+
+public class NotifierBot extends ListenerAdapter<PircBotX> {
+
+	private static final Log LOG = LogFactory.getLog(NotifierBot.class);
+
+	private final String message;
+
+	public NotifierBot(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public void onConnect(ConnectEvent<PircBotX> event) throws Exception {
+		LOG.debug("connected");
+	}
+
+	@Override
+	public void onDisconnect(DisconnectEvent<PircBotX> event) throws Exception {
+		LOG.debug("disconnected");
+		synchronized (this) {
+			this.notifyAll();
+		}
+	}
+
+	@Override
+	public void onJoin(JoinEvent<PircBotX> event) throws Exception {
+		LOG.debug("joined");
+		PircBotX pircBotX = event.getBot();
+		Channel channel = event.getChannel();
+		pircBotX.sendMessage(channel, this.message);
+		pircBotX.disconnect();
+	}
+}
