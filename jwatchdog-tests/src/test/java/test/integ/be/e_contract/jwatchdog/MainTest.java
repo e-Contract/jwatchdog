aa918afbfd302b80e34b6b207c961af21d7c0292
Frank Cornelis <info@e-contract.be>
2013-04-05 06:57:32 +0200

    file notifier

--- jwatchdog-tests/src/test/java/test/integ/be/e_contract/jwatchdog/MainTest.java
+++ jwatchdog-tests/src/test/java/test/integ/be/e_contract/jwatchdog/MainTest.java
@@ -253,4 +253,15 @@ public class MainTest {
 		Main.main(new String[] { path });
 		LOG.debug("end");
 	}
+
+	@Test
+	public void testNotifierFile() throws Exception {
+		URL configUrl = MainTest.class
+				.getResource("/jwatchdog-config-notifier-file.xml");
+		LOG.debug("config URL: " + configUrl);
+		String path = configUrl.toURI().getPath();
+		LOG.debug("path: " + path);
+		Main.main(new String[] { path });
+		LOG.debug("end");
+	}
 }
