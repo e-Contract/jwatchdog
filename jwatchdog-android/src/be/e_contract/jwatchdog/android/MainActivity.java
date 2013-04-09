890a9aa66e8be8a5b5f227b6809d21ccbfb106ce
Frank Cornelis <info@e-contract.be>
2013-04-09 08:31:27 +0200

    initial Android Maven project structure

---
+++ jwatchdog-android/src/be/e_contract/jwatchdog/android/MainActivity.java
@@ -0,0 +1,38 @@
+/*
+ * Java Watchdog Project.
+ * Copyright (C) 2013 Frank Cornelis.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.e_contract.jwatchdog.android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
---
+++ jwatchdog-android/src/be/e_contract/jwatchdog/android/SMSBroadcastReceiver.java
@@ -0,0 +1,43 @@
+/*
+ * Java Watchdog Project.
+ * Copyright (C) 2013 Frank Cornelis.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/.
+ */
+
+package be.e_contract.jwatchdog.android;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+public class SMSBroadcastReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		String action = intent.getAction();
+		if ("android.provider.Telephony.SMS_RECEIVED".equals(action)) {
+			Bundle bundle = intent.getExtras();
+			Object[] pdus = (Object[]) bundle.get("pdus");
+			for (Object pdu : pdus) {
+				SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdu);
+				Toast.makeText(context, smsMessage.getDisplayMessageBody(),
+						Toast.LENGTH_LONG).show();
+			}
+		}
+	}
+}
