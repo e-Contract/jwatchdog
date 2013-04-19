633c6ff01199a69d0ebd9f5fea1398d81aba100b
Frank Cornelis <info@e-contract.be>
2013-04-19 15:39:05 +0200

    RRD fix: close file

--- jwatchdog-datasource-rrd/src/main/java/be/e_contract/jwatchdog/datasource/rrd/RRDDatasource.java
+++ jwatchdog-datasource-rrd/src/main/java/be/e_contract/jwatchdog/datasource/rrd/RRDDatasource.java
@@ -20,7 +20,6 @@ package be.e_contract.jwatchdog.datasource.rrd;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Date;
 import java.util.Set;
 
 import org.apache.commons.logging.Log;
@@ -58,54 +57,62 @@ public class RRDDatasource implements Datasource {
 		} catch (IOException e) {
 			throw new RuntimeException("RRD IO error: " + e.getMessage());
 		}
-
-		DateTime lastUpdate = new DateTime(rrd.getLastUpdate());
-		LOG.debug("last update: " + lastUpdate);
-		DateTime now = new DateTime();
-		if (lastUpdate.isBefore(now.minusMinutes(minutes))) {
-			LOG.warn("RRD outdated");
-		}
-
-		Header header = rrd.getHeader();
-		int primaryDataPointInterval = header.getPDPStep();
-		DateTime endDateTime = lastUpdate
-				.minusSeconds(primaryDataPointInterval);
-		DateTime startDateTime = endDateTime.minusMinutes(minutes);
-		DataChunk dataChunk;
 		try {
-			dataChunk = rrd.getData(ConsolidationFunctionType.AVERAGE,
-					startDateTime.toDate(), endDateTime.toDate(),
-					primaryDataPointInterval);
-		} catch (IOException e) {
-			throw new RuntimeException("RRD IO error: " + e.getMessage());
-		}
-		double[][] data = dataChunk.getData();
-		Set<String> dataSourceNames = rrd.getDataSourcesName();
-		LOG.debug("RRD datasources: " + dataSourceNames);
 
-		int dsIdx;
-		if (null != this.datasourceName) {
-			if (!dataSourceNames.contains(this.datasourceName)) {
-				LOG.warn("RRD datasource name not found: "
-						+ this.datasourceName);
-				return new double[] {};
+			DateTime lastUpdate = new DateTime(rrd.getLastUpdate());
+			LOG.debug("last update: " + lastUpdate);
+			DateTime now = new DateTime();
+			if (lastUpdate.isBefore(now.minusMinutes(minutes))) {
+				LOG.warn("RRD outdated");
 			}
-			int size = dataSourceNames.size();
-			for (dsIdx = 0; dsIdx < size; dsIdx++) {
-				DataSource dataSource = rrd.getDataSource(dsIdx);
-				if (dataSource.getName().equals(this.datasourceName)) {
-					break;
+
+			Header header = rrd.getHeader();
+			int primaryDataPointInterval = header.getPDPStep();
+			DateTime endDateTime = lastUpdate
+					.minusSeconds(primaryDataPointInterval);
+			DateTime startDateTime = endDateTime.minusMinutes(minutes);
+			DataChunk dataChunk;
+			try {
+				dataChunk = rrd.getData(ConsolidationFunctionType.AVERAGE,
+						startDateTime.toDate(), endDateTime.toDate(),
+						primaryDataPointInterval);
+			} catch (IOException e) {
+				throw new RuntimeException("RRD IO error: " + e.getMessage());
+			}
+			double[][] data = dataChunk.getData();
+			Set<String> dataSourceNames = rrd.getDataSourcesName();
+			LOG.debug("RRD datasources: " + dataSourceNames);
+
+			int dsIdx;
+			if (null != this.datasourceName) {
+				if (!dataSourceNames.contains(this.datasourceName)) {
+					LOG.warn("RRD datasource name not found: "
+							+ this.datasourceName);
+					return new double[] {};
 				}
+				int size = dataSourceNames.size();
+				for (dsIdx = 0; dsIdx < size; dsIdx++) {
+					DataSource dataSource = rrd.getDataSource(dsIdx);
+					if (dataSource.getName().equals(this.datasourceName)) {
+						break;
+					}
+				}
+			} else {
+				dsIdx = 0;
 			}
-		} else {
-			dsIdx = 0;
-		}
 
-		double[] values = new double[data.length];
-		for (int idx = 0; idx < data.length; idx++) {
-			values[data.length - idx - 1] = data[idx][dsIdx];
+			double[] values = new double[data.length];
+			for (int idx = 0; idx < data.length; idx++) {
+				values[data.length - idx - 1] = data[idx][dsIdx];
+			}
+			return values;
+		} finally {
+			try {
+				rrd.close();
+			} catch (IOException e) {
+				LOG.error("error closing RRD: " + e.getMessage());
+			}
 		}
-		return values;
 	}
 
 	@Override
